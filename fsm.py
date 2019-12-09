d93a616501fa1d08f03d2fd470a2a0792a8f318f
f74062133 <lingxuanying@gmail.com>
2019-12-09 09:24:32 -0500

    commit

--- fsm.py
+++ fsm.py
@@ -21,6 +21,10 @@ class TocMachine(GraphMachine):
         text = event.message.text
         return text.lower() == "fsm"
 
+    def is_going_to_ask(self, event):
+        text = event.message.text
+        return text.lower() == "睡覺睡到自然醒" or text.lower() == "成為荒野女神" or text.lower() == "以上皆是"
+
     def on_enter_state1(self, event):
         print("I'm entering state1")
 
@@ -52,8 +56,6 @@ class TocMachine(GraphMachine):
         else:
             send_image(reply_token, "https://i.imgur.com/Xhk0Ztk.jpg")
 
-        self.go_back()
-
     def on_exit_state2(self):
         print("Leaving state2")
 
@@ -66,3 +68,21 @@ class TocMachine(GraphMachine):
 
     def on_exit_fsm(self):
         print("Leaving fsm")
+    def on_enter_ask(self, event):
+        print("I'm entering state1")
+
+        reply_token = event.reply_token
+
+        temp = random.randint(1,5)
+        if(temp==1):
+            send_image(reply_token, "https://i.imgur.com/Crfclsh.jpg")
+        elif(temp==2):
+            send_image(reply_token, "https://i.imgur.com/RPNUZHq.jpg")
+        elif(temp==3):
+            send_image(reply_token, "https://i.imgur.com/xdxCt6b.jpg")
+        else:
+            send_image(reply_token, "https://i.imgur.com/TmZEeKT.jpg")
+        self.go_back()
+
+    def on_exit_ask(self):
+        print("Leaving state1")
