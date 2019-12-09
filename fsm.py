fc42a27977176893f5ba7abe311c81d1985802f4
f74062133 <lingxuanying@gmail.com>
2019-12-09 08:21:42 -0500

    commit

--- fsm.py
+++ fsm.py
@@ -2,6 +2,7 @@ from transitions.extensions import GraphMachine
 
 from utils import send_text_message
 from utils import send_image
+from utils import button
 import random
 
 
--- utils.py
+++ utils.py
@@ -1,8 +1,7 @@
 import os
 
 from linebot import LineBotApi, WebhookParser
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, ImageMessage
-
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, ImageMessage, TemplateSendMessage
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
 
@@ -17,9 +16,34 @@ def send_image(reply_token, text):
     line_bot_api.reply_message(reply_token,ImageSendMessage(
 		original_content_url = text,
 		preview_image_url = text
-	    )
-	)
-
+            )
+        )
+    return "OK"
+def button(reply_token):
+    line_bot_api = LineBotApi(channel_access_token)
+    line_bot_api.reply_message(reply_token,ImageSendMessage(
+                altText = "Example buttons template", template = ButtonsTemplate(
+                            thumbnailImageUrl = "https://api.reh.tw/line/bot/example/assets/images/example.jpg", 
+                            title = "Example Menu", 
+                            text = "Please select", 
+                            actions = [
+                                        PostbackAction(
+                                                    display_text = "postback text", 
+                                                    label = "postback",
+                                                    data = "action=buy&itemid=123"  
+                                        ),
+                                        MessageAction( 
+                                                    label = "message",
+                                                    text = "Message example"  
+                                        ),
+                                        URIAction(
+                                                    label = "uri",
+                                                    uri = "https://github.com/GoneTone/line-example-bot-php" 
+                                        )
+                            ]
+                )
+            )
+        )
     return "OK"
 
 """
